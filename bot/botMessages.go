5325d37b6bef6ece631f86961c7e9258326326a2
Andy Ka_ban <ka_ban@rambler.ru>
2019-11-10 21:41:16 +0300

    fix bot dialog messages

--- bot/botMessages.go
+++ bot/botMessages.go
@@ -4,7 +4,7 @@ const (
 
 	botHello = `Hi, I'm Dimka Sodomit.
 My asshole has learned to handle photos like a Polaroid camera.
-Please send me a photo and I will do the magic with my ass`
+Please send me a photo and I will do the magic by my ass`
 
-	botHelp = `Please send me a photo and i will make a polaroid picture with using my ass`
+	botHelp = `Please send me a photo and i will make a polaroid picture by use my ass`
 )
